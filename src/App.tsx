d1167e237be75e70e1beeae8ec1aa6bdd8327744
hkwaller <hkwaller@gmail.com>
2020-07-02 06:45:21 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { motion } from 'framer-motion'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">hkwaller.</header>
+    </div>
+  )
+}
+
+export default App
